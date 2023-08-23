3c8795180088f42bce0f012689c231e2201db40a
unknown <melikamoslehoffical@gmail.com>
2023-08-23 14:18:01 +0330

    api

--- src/header/index.js
+++ src/header/index.js
@@ -1,21 +1,20 @@
 import html from "./index.html";
 import "./index.scss";
-import {checkWeather} from "./searchAPI";
+import { checkWeather } from "./searchAPI";
 
 function loaderPage() {
-
   const searchBox = document.querySelector(".header input");
   const searchBtn = document.querySelector(".header button");
- 
+
   searchBtn.addEventListener("click", () => {
-    checkWeather(searchBox.value);
     const originalColor = window.getComputedStyle(searchBtn).backgroundColor;
     searchBtn.style.backgroundColor = "rgba(255, 255, 255, 0.596)";
-    searchBtn.style.padding= "12px 17px";
+    searchBtn.style.padding = "12px 17px";
     setTimeout(() => {
       searchBtn.style.backgroundColor = originalColor;
-      searchBtn.style.padding= "10px 15px";
+      searchBtn.style.padding = "10px 15px";
     }, 200);
+    checkWeather(searchBox.value);
   });
   const keyUp = (event) => {
     if (event.key === "Enter") {
--- src/header/searchAPI.js
+++ src/header/searchAPI.js
@@ -2,7 +2,7 @@ const apiKey = "886705b4c1182eb1c69f28eb8c520e20";
 const apiUrl = "https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
 
 export async function checkWeather(city) {
-    const response = await fetch(apiUrl + city + `&appid=${apiKey}` + "°");
+    const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     var data = await response.json();
 
     console.log(data);
