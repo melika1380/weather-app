eca4fac0683f300f623b862b3608d5e17eb65530
unknown <melikamoslehoffical@gmail.com>
2023-09-02 16:07:17 +0330

    edit

--- src/header/index.js
+++ src/header/index.js
@@ -3,9 +3,12 @@ import "./index.scss";
 import { checkWeather } from "./searchAPI";
 
 function loaderPage() {
+
   const searchBox = document.querySelector(".input");
   const searchBtn = document.querySelector(".header button");
 
+  checkWeather("tehran");
+  
   searchBtn.addEventListener("click", () => {
     checkWeather(searchBox.value);
     searchBox.value = "";
